ef0014422dc4cbbff739f9a84eb4d01edfb83642
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2023-08-30 09:50:59 -0700

    added the card and pushed the object in to an array

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 /** @format */
 
-const myLibrary = [];
+const myLibrary = []; //declared an empty array to push the objects
+const plusButton = document.getElementById("plus_icon");
+const displayArea = document.getElementById("d1");
+let inputValues = document.querySelectorAll('input[type="text"]');
+
+//constructor for adding new book
 
 function addNewBook(title, author, pages, alreadyRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.alreadyRead = alreadyRead;
-
-  this.showInformation = function () {
-    let result = `The book ${title} by ${author} has ${pages} and it is ${alreadyRead} that you already read the book.`; //best pratice to return the function and cosnole log it after calling the function
-
-    return result;
-  };
 }
 
-const newBook = new addNewBook("Harry Potter", "JK Rowling", "500", "yes");
-newBook.showInformation();
-console.log(newBook.showInformation());
+//adding a card on button click and also getting the values from the UI and sending it to the constructor
+plusButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const titleUi = document.getElementById("book_title").value;
+  const authorUi = document.getElementById("book_author").value;
+  const no_of_pagesUi = document.getElementById("book_pages").value;
+  const alreadyReadUi = document.getElementById("book_read").value;
+  addCard();
+
+  //instantiating the constructor by creating an instance called newBook
+  const newBook = new addNewBook(
+    titleUi,
+    authorUi,
+    no_of_pagesUi,
+    alreadyReadUi
+  );
+
+  //pushing the new object inside the array
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+});
+
+addCard = () => {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("card");
+
+  displayArea.insertAdjacentElement("beforeend", newDiv);
+};
 
 function addBookToTheLibrary() {}
