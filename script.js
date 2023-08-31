d003b9625849ae3beb08db49b6e539be09f30b54
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2023-08-30 21:47:23 -0700

    appended the card with the book details

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const myLibrary = []; //declared an empty array to push the objects
 const plusButton = document.getElementById("plus_icon");
 const displayArea = document.getElementById("d1");
 let inputValues = document.querySelectorAll('input[type="text"]');
+let counter = -1;
+const clearButton = document.getElementById("clear_data");
 
 //constructor for adding new book
 
@@ -16,32 +18,68 @@ function addNewBook(title, author, pages, alreadyRead) {
 
 //adding a card on button click and also getting the values from the UI and sending it to the constructor
 plusButton.addEventListener("click", (event) => {
+  counter = counter + 1;
+
   event.preventDefault();
 
   const titleUi = document.getElementById("book_title").value;
   const authorUi = document.getElementById("book_author").value;
   const no_of_pagesUi = document.getElementById("book_pages").value;
-  const alreadyReadUi = document.getElementById("book_read").value;
-  addCard();
+  /*const alreadyReadUi = document.getElementById("book_isRead").value;*/
+  const aRead = document.querySelector("#book_isRead").checked;
+  console.log(aRead);
 
   //instantiating the constructor by creating an instance called newBook
-  const newBook = new addNewBook(
-    titleUi,
-    authorUi,
-    no_of_pagesUi,
-    alreadyReadUi
-  );
+  const newBook = new addNewBook(titleUi, authorUi, no_of_pagesUi, aRead);
 
   //pushing the new object inside the array
   myLibrary.push(newBook);
-  console.log(myLibrary);
+  addCard(myLibrary, counter);
+  clearForm(inputValues);
 });
 
-addCard = () => {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("card");
+addCard = (mL, cV) => {
+  //console.log(mL);
+  let titles = mL.map((L) => L.title);
+  let authors = mL.map((L) => L.author);
+  let pages = mL.map((L) => L.pages);
+  let readStatus = mL.map((L) => L.alreadyRead);
+  console.log(cV);
+  const bookCard = document.createElement("div");
 
-  displayArea.insertAdjacentElement("beforeend", newDiv);
+  bookCard.classList.add("card");
+  bookCard.setAttribute("id", "newDiv");
+
+  const titleDetail = document.createElement("p");
+  const authorDetail = document.createElement("p");
+  const pageDetail = document.createElement("p");
+  const readStatusDetail = document.createElement("p");
+  const removeButton = document.createElement("button");
+
+  titleDetail.textContent = `Title:${titles[cV]}`;
+  authorDetail.textContent = `Author:${authors[cV]}`;
+  pageDetail.textContent = `Pages:${pages[cV]}`;
+  readStatusDetail.textContent = `Already read?:${readStatus[cV]}`;
+  removeButton.textContent = "Remove";
+
+  bookCard.appendChild(titleDetail);
+
+  bookCard.appendChild(authorDetail);
+
+  bookCard.appendChild(pageDetail);
+  bookCard.appendChild(readStatusDetail);
+  bookCard.append(removeButton);
+
+  displayArea.insertAdjacentElement("beforeend", bookCard);
 };
 
-function addBookToTheLibrary() {}
+clearButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  clearForm(inputValues);
+});
+
+clearForm = (ipV) => {
+  ipV.forEach((ip) => {
+    ip.value = "";
+  });
+};
